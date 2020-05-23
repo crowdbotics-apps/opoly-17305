519a11321496569a26eaeccf23aa781f8470e5c7
Crowdbotics <team@crowdbotics.com>
2020-05-23 07:53:50 +0000

    Update models

--- backend/opoly_17305/wsgi.py
+++ backend/opoly_17305/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opoly_17305.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opoly_17305.settings")
 
 application = get_wsgi_application()
